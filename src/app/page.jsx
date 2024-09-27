9e25f36ddfc693c6000378d22d2314bb4944ca48
KalashT <kalashthakare898@gmail.com>
2024-09-27 17:26:52 +0530

    commit 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -72,25 +72,80 @@ export default function Home() {
       </div>
 
 
-      <div className="grid grid-rows-3 ">
+      <div className="grid grid-rows-3 m-5">
         <div className="">
             <div className="flex text-center justify-center items-center">
               <h4 className="text-[#5F5F5F] text-[50px] font-semibold">
                 Our Mission
               </h4>
             </div>
-          <div className="flex justify-center items-center"> 
-              <div className="flex justify-start">
+          <div className="grid grid-cols-2"> 
+              <div className="flex justify-center items-center">
                 <img src="/icons/mission.jpg" alt="" />
               </div>
-              <div className="justify-end text-right">
-                <p>
-                  Our mission involves community engagement, leadership development, building strong tech foundation, while enabling all tech enthusiasts to contribute to the global society.
-                </p>
+              <div className="flex items-center justify-end text-right">
+                <div className="pr-5 border-r-[12px] border-[#4285F4] rounded-lg">
+                  <p className="text-[30px] text-[#5F5F5F] font-[500] justify">
+                    Our mission involves community engagement, leadership development, building strong tech foundation, while enabling all tech enthusiasts to contribute to the global society.
+                  </p>
+                </div>  
               </div>
           </div>
         </div>
-        
+        <div className="">
+            <div className="flex text-center justify-center items-center">
+              <h4 className="text-[#5F5F5F] text-[50px] font-semibold">
+                Our Perspective
+              </h4>
+            </div>
+          <div className="grid grid-cols-2">  
+              <div className="flex items-center justify-end text-left">
+                <div className="pl-5 border-l-[12px] border-[#0F9D56] rounded-lg">
+                  <p className="text-[30px] text-[#5F5F5F] font-[500] justify">
+                    We’re a community-driven initiative aiming to bridge the gap between research and practice , develop evolutionary thinking and network throughout the process. We believe in connecting fellow developers , spreading stimulative ideas and working for a solution driven team.
+                  </p>
+                </div>  
+              </div>
+              <div className="flex justify-center items-center">
+                <img src="/icons/perss.png" alt="" />
+              </div>
+          </div>
+        </div>
+        <div className="">
+            <div className="flex text-center justify-center items-center">
+              <h4 className="text-[#5F5F5F] text-[50px] font-semibold">
+                What Keeps Us Going?
+              </h4>
+            </div>
+          <div className="grid grid-cols-2"> 
+              <div className="flex justify-center items-center">
+                <img src="/icons/going.png" alt="" />
+              </div>
+              <div className="flex items-center justify-end text-right">
+                <div className="pr-5 border-r-[12px] border-[#FFC727] rounded-lg">
+                  <p className="text-[30px] text-[#5F5F5F] font-[500] justify">
+                    Our club helps students to connect, learn, empower and grow. Teamwork, innovative thinking, communication, and leading with solutions is what helps us achieve new heights.
+                    The entire team works in coordination, to inspire and motivate the upcoming coding community to evolve their skills and broaden their horizons of knowledge.
+                  </p>
+                </div>  
+              </div>
+          </div>
+        </div>
+
+
+
+
+        <div>
+          <div className="grid grid-cols-4  h-[210px]">
+            <div className="flex justify-center items-center">
+              <div className="bg-[url('/icons/Group5.jpg')] bg-auto bg-center w-full flex justify-center items-center">
+                <h3 className="flex justify-center items-center font-bold text-[75px]">
+                  03+
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       </section>
